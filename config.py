4189de7f462d8c07d8a325267129a267fabd6caf
hcuk94 <me@hc.me.uk>
2021-12-27 22:39:51 +0000

    did most of the things

---
+++ config.py
@@ -0,0 +1,14 @@
+# ###################################
+# Teltonika Cell Autolock Script
+# ###################################
+
+# To use, firstly configure your cell earcfn and pcid values below:
+cell_earcfn = 1392
+cell_pcid = 365
+# Then, add a cron job to run main.py (e.g. hourly).
+# That's all there is to it
+# Additional configurable options are shown below.
+
+log_file = 'main.log'  # File to log to
+log_level = 10  # 0 NOTSET, 10 DEBUG, 20 INFO, 30 WARNING, 40 ERROR, 50 CRITICAL
+
