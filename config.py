c845b3f2e27d08c5bd9f8976e7df8b43a6830c6f
hcuk94 <me@hc.me.uk>
2021-12-27 23:10:17 +0000

    fixed typo where I was using earcfn instead of earfcn

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Teltonika Cell Autolock Script
 # ###################################
 
-# To use, firstly configure your cell earcfn and pcid values below:
-cell_earcfn = 1392
+# To use, firstly configure your cell earfcn and pcid values below:
+cell_earfcn = 1392
 cell_pcid = 365
 # Then, add a cron job to run main.py (e.g. hourly).
--- main.py
+++ main.py
@@ -8,32 +8,27 @@ current_cell_data_string = current_cell_cmd.read()
 
 # Put cell info string into a list
 current_cell_data_list = current_cell_data_string.split(",")
-current_earcfn = int(current_cell_data_list[8])
+current_earfcn = int(current_cell_data_list[8])
 current_pcid = int(current_cell_data_list[7])
 
 # Get desired cell info
-desired_earcfn = config.cell_earcfn
+desired_earfcn = config.cell_earfcn
 desired_pcid = config.cell_pcid
 
-# Compare earcfn/pcid
-if current_pcid == desired_pcid and current_earcfn == desired_earcfn:
-    # earcfn/pcid matches, log this and take no further action
-    print("Current EARCFN/PCID values of {}/{} match desired values of {}/{}. No further action will be taken."
-          .format(current_earcfn, current_pcid, desired_earcfn, desired_pcid))
+# Compare earfcn/pcid
+if current_pcid == desired_pcid and current_earfcn == desired_earfcn:
+    # earfcn/pcid matches, log this and take no further action
+    print("Current EARFCN/PCID values of {}/{} match desired values of {}/{}. No further action will be taken."
+          .format(current_earfcn, current_pcid, desired_earfcn, desired_pcid))
 else:
-    # earcfn/pcid does not match, so lets run the sequence to lock cell
-    print("Current EARCFN/PCID values of {}/{} do not match desired values of {}/{}. Cell lock sequence will be run..."
-          .format(current_earcfn, current_pcid, desired_earcfn, desired_pcid))
+    # earfcn/pcid does not match, so lets run the sequence to lock cell
+    print("Current EARFCN/PCID values of {}/{} do not match desired values of {}/{}. Cell lock sequence will be run..."
+          .format(current_earfcn, current_pcid, desired_earfcn, desired_pcid))
     mode_override_cmd = "gsmctl -A 'AT+QCFG=\"NWSCANMODE\",3,1'"
     mode_override_cmd_run = os.popen(mode_override_cmd)
     print("Mode Override: " + mode_override_cmd_run.read())
     sleep(1)
-    cell_lock_cmd = "gsmctl -A 'AT+QNWLOCK=\"common/lte\",2,{},{}'".format(desired_earcfn, desired_pcid)
+    cell_lock_cmd = "gsmctl -A 'AT+QNWLOCK=\"common/lte\",2,{},{}'".format(desired_earfcn, desired_pcid)
     cell_lock_cmd_run = os.popen(cell_lock_cmd)
     print("Cell Lock: " + cell_lock_cmd_run.read())
     print("Cell lock sequence complete.")
-
-
-
-
-
