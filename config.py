9c533a23461530c48639d7e7f654512514905c64
hcuk94 <me@hc.me.uk>
2021-12-28 00:23:43 +0000

    updated to use logging module again

--- config.py
+++ config.py
@@ -6,3 +6,8 @@
 cell_earfcn = 1392
 cell_pcid = 365
 # Then, add a cron job to run main.py (e.g. hourly).
+# For full install instructions, see README.md
+# Additional configurable options are shown below.
+
+log_file = 'main.log'  # File to log to
+log_level = 10  # 0 NOTSET, 10 DEBUG, 20 INFO, 30 WARNING, 40 ERROR, 50 CRITICAL
--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 import os
 import config
+import logging
 from time import sleep
 
+# Configure logging using config file
+ logging.basicConfig(level=config.log_level, filename=config.log_file
+                     , filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
+
 # Get current cell info
+logging.debug("Getting current cell info...")
 current_cell_cmd = os.popen('gsmctl -K')
 current_cell_data_string = current_cell_cmd.read()
+logging.debug("Got cell info string: " + current_cell_data_string)
 
 # Put cell info string into a list
+logging.debug("Extracting cell info...")
 current_cell_data_list = current_cell_data_string.split(",")
 current_earfcn = int(current_cell_data_list[8])
 current_pcid = int(current_cell_data_list[7])
+logging.debug("Current EARCFN: " + str(current_earfcn))
+logging.debug("Current PCID: " + str(current_pcid))
 
 # Get desired cell info
 desired_earfcn = config.cell_earfcn
 desired_pcid = config.cell_pcid
+logging.debug("Desired EARCFN: " + desired_earfcn)
+logging.debug("Desired PCID: " + desired_pcid)
 
 # Compare earfcn/pcid
 if current_pcid == desired_pcid and current_earfcn == desired_earfcn:
     # earfcn/pcid matches, log this and take no further action
-    print("Current EARFCN/PCID values of {}/{} match desired values of {}/{}. No further action will be taken."
+    logging.info("Current EARFCN/PCID values of {}/{} match desired values of {}/{}. No further action will be taken."
           .format(current_earfcn, current_pcid, desired_earfcn, desired_pcid))
 else:
     # earfcn/pcid does not match, so lets run the sequence to lock cell
-    print("Current EARFCN/PCID values of {}/{} do not match desired values of {}/{}. Cell lock sequence will be run..."
+    logging.info("Current EARFCN/PCID values of {}/{} do not match desired values of {}/{}. Cell lock sequence will be run..."
           .format(current_earfcn, current_pcid, desired_earfcn, desired_pcid))
     mode_override_cmd = "gsmctl -A 'AT+QCFG=\"NWSCANMODE\",3,1'"
+    logging.debug("Running command: " + mode_override_cmd)
     mode_override_cmd_run = os.popen(mode_override_cmd)
-    print("Mode Override: " + mode_override_cmd_run.read())
+    logging.debug("Output: " + mode_override_cmd_run.read())
     sleep(1)
     cell_lock_cmd = "gsmctl -A 'AT+QNWLOCK=\"common/lte\",2,{},{}'".format(desired_earfcn, desired_pcid)
+    logging.debug("Running command: " + cell_lock_cmd)
     cell_lock_cmd_run = os.popen(cell_lock_cmd)
-    print("Cell Lock: " + cell_lock_cmd_run.read())
-    print("Cell lock sequence complete.")
+    logging.debug("Output: " + cell_lock_cmd_run.read())
+    logging.info("Cell lock sequence complete.")
