894bdf53a4f5e52355cb2c05591b78b160544184
hcuk94 <me@hc.me.uk>
2022-01-14 14:19:21 +0000

    updated stuff to use logging func

--- autolock.sh
+++ autolock.sh
@@ -20,9 +20,6 @@ logger() {
 # Main Program                                                                                                                                                                                              
 ##################                                                                                                                                                                                          
 
-# Set Datetime of App Run
-run_datetime=$(date +'%F %R')
-                                                                                                                                                                                                            
 # Get currently connected cell, and extrapolate current earfcn/pcid                                                                                                                                         
 current_cell=`gsmctl -K`                                                                                                                                                                                    
 current_cell_data_list=$(echo $current_cell | tr "," "\n")                                                                                                                                                  
@@ -35,18 +32,18 @@ current_pcid=`echo -e "$current_cell_data_list" | sed -n '8p'`
 if [[ $current_earfcn == $DESIRED_EARFCN && $current_pcid == $DESIRED_PCID ]]; then                                       
     logger "Current EARFCN/PCID of $current_earfcn/$current_pcid match desired $DESIRED_EARFCN/$DESIRED_PCID. No action will be taken."
 else                                                                                                                                                                                                     
-    echo "$run_dateiCurrent EARFCN/PCID of $current_earfcn/$current_pcid do not match desired $DESIRED_EARFCN/$DESIRED_PCID. Running cell lock sequence..."                      
+    logger "$run_dateiCurrent EARFCN/PCID of $current_earfcn/$current_pcid do not match desired $DESIRED_EARFCN/$DESIRED_PCID. Running cell lock sequence..."                      
                                                                                                                                                                                                             
     # Main Cell Locking Sequence                                                                                                                                                                            
     force_scanmode=`gsmctl -A 'AT+QCFG="NWSCANMODE",3,1'`                                                                                                                                                   
     if [[ $force_scanmode == *"OK"* ]]; then                                                                                                                                                                
         lock_cell=$(gsmctl -A 'AT+QNWLOCK=\"common/lte\",2,$DESIRED_EARFCN,$DESIRED_PCID')                                                                                                                  
         if [[ $lock_cell == *"OK"* ]]; then                                                                                                                                                                 
-            echo "Cell lock completed successfully"                                                                                                                                                         
+            logger "Cell lock completed successfully"                                                                                                                                                         
         else                                                                                                                                                                                                
-            echo "An error was encountered while locking the cell"                                                                                                                                          
+            logger "An error was encountered while locking the cell"                                                                                                                                          
         fi                                                                                                                                                                                                  
     else                                                                                                                                                                                                    
-        echo "An error was encountered while setting the scanmode to LTE only"                                                                                                                              
+        logger "An error was encountered while setting the scanmode to LTE only"                                                                                                                              
     fi                                                                                                                                                                                                      
 fi
